5dbe7ffd704cbe7991f43a638d3b92b6eb5e3d0a
Chinomchino <58937034+Chinomchino@users.noreply.github.com>
2025-11-11 14:43:37 +0200

    Update NVDisplaykernel.cpp

--- NVDisplaykernel.cpp
+++ NVDisplaykernel.cpp
@@ -1,15 +1,13 @@
-#define SKIP_DRIVERKIT 1
-
 #include <IOKit/IOLib.h>
-#include <IOKit/pci/IOPCIDevice.h>
+#include <IOKit/IOService.h>
 #include <IOKit/IOBufferMemoryDescriptor.h>
 
 #define super IOService
+
 class NVDisplay : public IOService {
     OSDeclareDefaultStructors(NVDisplay)
-    
 private:
-    IOPCIDevice* device = nullptr;
+    IOService* providerDevice = nullptr;
 
 public:
     virtual bool start(IOService* provider) override;
@@ -21,20 +19,15 @@ OSDefineMetaClassAndStructors(NVDisplay, IOService)
 bool NVDisplay::start(IOService* provider) {
     IOLog("NVDisplayKext: starting\n");
 
-    device = OSDynamicCast(IOPCIDevice, provider);
-    if (!device) {
-        IOLog("NVDisplayKext: provider is not a PCI device\n");
-        return false;
-    }
-
-    device->retain();
-    device->open(this);
+    providerDevice = provider;
+    providerDevice->retain();
+    providerDevice->open(this);
 
     UInt16 vendor = 0, deviceId = 0;
 
-    // Modern macOS 15+ way to read PCI config space
-    if (device->getDeviceMemoryCount() > 0) {
-        IOMemoryMap* cfgMap = device->mapDeviceMemoryWithIndex(0);
+    // Read PCI config space via first BAR mapping
+    if (providerDevice->getDeviceMemoryCount() > 0) {
+        IOMemoryMap* cfgMap = providerDevice->mapDeviceMemoryWithIndex(0);
         if (cfgMap) {
             volatile UInt8* cfg = (volatile UInt8*)cfgMap->getVirtualAddress();
             vendor = *(volatile UInt16*)(cfg + 0x00);
@@ -45,16 +38,7 @@ bool NVDisplay::start(IOService* provider) {
 
     IOLog("NVDisplayKext: vendor=0x%x device=0x%x\n", vendor, deviceId);
 
-    // Map first BAR
-    IOMemoryMap* mmio = device->mapDeviceMemoryWithIndex(0);
-    if (mmio) {
-        volatile UInt32* regs = (volatile UInt32*)mmio->getVirtualAddress();
-        IOLog("NVDisplayKext: Mapped BAR0 @ %p, first reg=0x%08x\n", regs, regs[0]);
-    } else {
-        IOLog("NVDisplayKext: Failed to map BAR0\n");
-    }
-
-    // Allocate dummy framebuffer
+    // Allocate fake framebuffer
     IOBufferMemoryDescriptor* fb = IOBufferMemoryDescriptor::inTaskWithOptions(
         kernel_task,
         kIOMemoryKernelUserShared,
@@ -75,11 +59,10 @@ bool NVDisplay::start(IOService* provider) {
 
 void NVDisplay::stop(IOService* provider) {
     IOLog("NVDisplayKext: stopping\n");
-    if (device) {
-        device->close(this);
-        device->release();
-        device = nullptr;
+    if (providerDevice) {
+        providerDevice->close(this);
+        providerDevice->release();
+        providerDevice = nullptr;
     }
     super::stop(provider);
 }
-
